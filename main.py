70e0f947b5fc5032bbd66829ad15d9262e9297ca
Arjun Goel <arjungoel1995@gmail.com>
2017-08-16 22:08:42 +0530

    new lines added.

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("Let's get started!")
 spy_name=input("Provide your name here:")
+spy_salutation=input("What should we call you")
+print("Welcome" + spy_name + "Glad to have you back with us.")

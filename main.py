9c1425dede086c05be8cfc9020947c444954f5cb
Arjun Goel <arjungoel1995@gmail.com>
2017-08-16 22:45:47 +0530

    new statements added.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print("Let's get started!")
-spy_name=input("Provide your name here:")
-spy_salutation=input("What should we call you")
-print("Welcome" + spy_name + "Glad to have you back with us.")
+spy_name=input("Provide your name here :")
+spy_salutation=input("What should we call you :")
+# check whether spy has input something or not
+if len(spy_name) > 0:
+    #code block if the condition is true.
+# concatenation of salutation and name
+spy_name=(spy_salutation  +  " "  +  spy_name)
+print('Welcome'  + spy_name +  "Glad to have you back with us.")

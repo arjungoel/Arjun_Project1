884051b419c3d520478aa376a7ec9fd9f49871d5
Arjun Goel <arjungoel1995@gmail.com>
2017-08-18 22:28:45 +0530

    new lines insertion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import statements
 from spy_details import spy_name,spy_salutation,spy_age,spy_rating
+from start_chat import start_chat
 print("Let's get started!")
 question=("Do you want to continue as" + spy_salutation + " " + spy_name + "(Y/N) : ")
 existing=input(question)
@@ -7,10 +8,9 @@ existing=input(question)
 # validating user's input
 if (existing == 'Y' or existing == 'y') :
     # logic here.
-    pass
+    start_chat(spy_name,spy_age,spy_rating)
 elif (existing == 'N' or existing == 'n') :
     # new users code here.
-    pass
     spy_name = input("Provide your name here :")
     # check whether spy has input something or not
     if len(spy_name) > 0:
--- start_chat.py
+++ start_chat.py
@@ -1,10 +1,14 @@
 # start_chat() function definition..
-def start_chat():
-    menu_choices = ("What do you want to do ? \n 1. Add Status")
-    result = input(menu_choices  )
-
-    # validating users input..
-    if (result == 1):
+def start_chat(name,age,rating):
+    show_menu=True
+    while(show_menu):
+        menu_choices = ("What do you want to do ? \n 1. Add Status \n 2. Close Application")
+        result = int(input(menu_choices))
+        # validating users input..
+        if (result == 1):
         # action 1
-    else:
-        print("Wrong choice. Try again.")
+            pass
+        elif (result == 2):
+            show_menu=False
+        else:
+            print("Wrong choice. Try again.")

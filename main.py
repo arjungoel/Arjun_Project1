407f4f4ff2ec0f5e5d5c09eb47441a13ebffc572
Arjun Goel <arjungoel1995@gmail.com>
2017-08-18 22:02:50 +0530

    function created.

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+# import statements
+from spy_details import spy_name,spy_salutation,spy_age,spy_rating
 print("Let's get started!")
-spy_name=input("Provide your name here :")
-# check whether spy has input something or not
-if len(spy_name) > 0:
-    #code block if the condition is true.
-    # concatenation of salutation and name
-    spy_age=0
-    spy_rating=0.0
-    spy_is_online=False
-    spy_salutation = input("What should we call you :")
-    spy_age=input("Enter your age. ?")
-    print (type(spy_age))
-    spy_age=int(spy_age)
-    print(spy_age)
-    spy_name = (spy_salutation + " " + spy_name)
-    print('Welcome'  + spy_name  +  "Glad to have you back with us.")
-    print("Alright"  + spy_name  +  "I would like to know little bit about you before we proceed.")
+question=("Do you want to continue as" + spy_salutation + " " + spy_name + "(Y/N) : ")
+existing=input(question)
+
+# validating user's input
+if (existing == 'Y' or existing == 'y') :
+    # logic here.
+    pass
+elif (existing == 'N' or existing == 'n') :
+    # new users code here.
+    pass
+    spy_name = input("Provide your name here :")
+    # check whether spy has input something or not
+    if len(spy_name) > 0:
+        # code block if the condition is true.
+        # concatenation of salutation and name
+        spy_age = 0
+        spy_rating = 0.0
+        spy_is_online = False
+        spy_salutation = input("What should we call you :")
+        spy_age = input("Enter your age. ?")
+        print(type(spy_age))
+        spy_age = int(spy_age)
+        print(spy_age)
+        spy_name = (spy_salutation + " " + spy_name)
+        print('Welcome' + spy_name + "Glad to have you back with us.")
+        print("Alright" + spy_name + "I would like to know little bit about you before we proceed.")
+    else:
+        print(" Invalid name. Try again.")
 else:
-    print(" Invalid name. Try again.")
+    print("Wrong choice. Try again.")
---
+++ start_chat.py
@@ -0,0 +1,10 @@
+# start_chat() function definition..
+def start_chat():
+    menu_choices = ("What do you want to do ? \n 1. Add Status")
+    result = input(menu_choices  )
+
+    # validating users input..
+    if (result == 1):
+        # action 1
+    else:
+        print("Wrong choice. Try again.")

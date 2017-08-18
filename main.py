29b0876fafe378a6340aae1d93895ce6bc7de81c
Arjun Goel <arjungoel1995@gmail.com>
2017-08-18 21:31:51 +0530

    new default user

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 print("Let's get started!")
 spy_name=input("Provide your name here :")
-spy_salutation=input("What should we call you :")
 # check whether spy has input something or not
 if len(spy_name) > 0:
     #code block if the condition is true.
     # concatenation of salutation and name
+    spy_age=0
+    spy_rating=0.0
+    spy_is_online=False
+    spy_salutation = input("What should we call you :")
+    spy_age=input("Enter your age. ?")
+    print (type(spy_age))
+    spy_age=int(spy_age)
+    print(spy_age)
     spy_name = (spy_salutation + " " + spy_name)
-    print('Welcome' + spy_name + "Glad to have you back with us.")
-    print("Alright" + spy_name + "I would like to know little bit about you before we proceed.")
+    print('Welcome'  + spy_name  +  "Glad to have you back with us.")
+    print("Alright"  + spy_name  +  "I would like to know little bit about you before we proceed.")
+else:
+    print(" Invalid name. Try again.")
